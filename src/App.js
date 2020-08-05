cda5f9d0fd0eaab259b4ba5cc698a02ff13e99ee
Vova <vova.champion@gmail.com>
2020-08-05 22:01:40 +0200

    Add logo and change header

--- src/App.js
+++ src/App.js
@@ -4,19 +4,16 @@ import './App.css';
 import Footer from './components/share/Footer';
 import Header from './components/share/Header';
 import Main from './components/Main';
-// import LeftLayout from './components/home/LeftLayout';
-// import MainContent from './components/home/MainContent';
 
 
 class MyApp extends Component {
   render() {
     return (
-      <div className="app" style={{fontFamily:"'Helvetica Neue', sans-serif"}}>
+      <div className="app">
+        {/* <div className="app" style={{fontFamily:"'Helvetica Neue', sans-serif"}}> */}
         <Header />
         <div className="container-fluid">
           <Main />
-          {/* <LeftLayout />
-          <MainContent />  */}
         </div>
         <Footer />
       </div>
--- src/components/share/Header.js
+++ src/components/share/Header.js
@@ -1,15 +1,46 @@
 import React from "react";
 
 import './Header.css';
+import MyLogo from '../../assets/vova-avatar.jpg';
+import Background from '../../assets/unsplash2.jpg';
 import Social from "../lib/Social";
+
+var sectionStyle = {
+  width:'100%',
+  // minHeight: '500px',
+  height:'100%',
+
+  /* Create the parallax scrolling effect */
+  backgroundAttachment: 'fixed',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+
+  backgroundImage:`url(${Background})`
+};
 
 function Header() {
   return (
-    <div className="header">
-      <div className="contact">
-        <div>My header</div>
+    <div style={ sectionStyle }>
+      <div className="header">
+        
+        <div className="logo">
+          <img src={MyLogo} alt="myphoto"/>
+        </div>
+
+        <div className="title">
+          <p> FRONTEND / WEB / NATIVE / FULLSTACK DEVELOPER </p>
+        </div>
+
+        <div className="contact">
+          <ul>
+            <li>Email: <a href="mailto:volodymyr.avramenko77@gmail.com">volodymyr.avramenko77@gmail.com</a></li>
+            <li>Phone: +46-070-2688977</li>
+          </ul>
+        </div>
+        <Social />
       </div>
-      <Social />
+    
     </div>
   );
 }
