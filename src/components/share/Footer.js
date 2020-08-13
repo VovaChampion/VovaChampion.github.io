970a5c83fcfbe9dcc21e3a74f295689bc44602ce
Vova <vova.champion@gmail.com>
2020-08-13 11:08:59 +0200

    Add contact me phrase

--- src/components/share/Footer.js
+++ src/components/share/Footer.js
@@ -3,13 +3,19 @@ import React from "react";
 import './Footer.css';
 import Social from "../lib/Social";
 import BackgroundImg from '../../assets/unsplash2.jpg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+  faHandshake
+} from '@fortawesome/free-solid-svg-icons';
 
 function MyFooter() {
   return (
     <div className="footer" style={{backgroundImage: "url(" + BackgroundImg + ")"}}>
       <Social />
       <div className="contact">
-        <div>Volodymyr Avramenko ©2020</div>
+        <p>Contact me via LinkedIn <FontAwesomeIcon className='icon' icon={faHandshake}/></p>
+        <p>Volodymyr Avramenko ©2020</p>
       </div>
     </div>
   );
