398842604597cd2066a5308ee97261362bfe2ab5
Vova <vova.champion@gmail.com>
2023-03-25 17:59:54 +0100

    Add build

---
+++ build/precache-manifest.451ba5a01e1f4e5cbf8b4fd222153a1c.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f30f81da451316ea2a0c177381e070f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b81b834614060172187",
+    "url": "/static/css/2.86abc55e.chunk.css"
+  },
+  {
+    "revision": "92591c39e0740d358bb3",
+    "url": "/static/css/main.76d0992e.chunk.css"
+  },
+  {
+    "revision": "0b81b834614060172187",
+    "url": "/static/js/2.bbd30b43.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bbd30b43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92591c39e0740d358bb3",
+    "url": "/static/js/main.ce4c64e5.chunk.js"
+  },
+  {
+    "revision": "e3473a344493b6ab086d",
+    "url": "/static/js/runtime-main.21e4bbbf.js"
+  },
+  {
+    "revision": "8bdfea264ed748038a5824e94468a351",
+    "url": "/static/media/BookSystem1.8bdfea26.png"
+  },
+  {
+    "revision": "8e01271d7227015c2fb516492e47d4cb",
+    "url": "/static/media/BookSystem2.8e01271d.png"
+  },
+  {
+    "revision": "3661cc6444d94bd8a15f704369ac0551",
+    "url": "/static/media/BookSystem3.3661cc64.png"
+  },
+  {
+    "revision": "936f8e809d5004e8116d9c4735e87281",
+    "url": "/static/media/BookSystem4.936f8e80.png"
+  },
+  {
+    "revision": "cb2125cea895a65dbccb52b2bfebadb3",
+    "url": "/static/media/BookSystem5.cb2125ce.png"
+  },
+  {
+    "revision": "1d7f7df5f9ea381b3021b8b71668da49",
+    "url": "/static/media/BusTicket1.1d7f7df5.png"
+  },
+  {
+    "revision": "24919e52b9f4941df431757a6a11c2aa",
+    "url": "/static/media/BusTicket2.24919e52.png"
+  },
+  {
+    "revision": "92b88c8aeefbe0906680990d44a20c92",
+    "url": "/static/media/BusTicket3.92b88c8a.png"
+  },
+  {
+    "revision": "ee7adadca8746920dd39d2e913e69d20",
+    "url": "/static/media/BusTicket4.ee7adadc.png"
+  },
+  {
+    "revision": "a8a58eec399bf9b29b9ff82c8774394f",
+    "url": "/static/media/BusTicket5.a8a58eec.png"
+  },
+  {
+    "revision": "0c261b8d3045f9005495ef425cc7443b",
+    "url": "/static/media/Magento1.0c261b8d.png"
+  },
+  {
+    "revision": "b15f267d412dd2fa5255912164ef675f",
+    "url": "/static/media/Magento2.b15f267d.png"
+  },
+  {
+    "revision": "ec8f71b5a0c704d6c0aaef6280f63735",
+    "url": "/static/media/Magento3.ec8f71b5.png"
+  },
+  {
+    "revision": "11d783537c01d14a8d8fd16780640224",
+    "url": "/static/media/Magento4.11d78353.png"
+  },
+  {
+    "revision": "f477f4de3056ce8606e2c947bb105a80",
+    "url": "/static/media/Magento5.f477f4de.png"
+  },
+  {
+    "revision": "8a7b1af28c7cfc3f7208440898dadee0",
+    "url": "/static/media/OnlineShop1.8a7b1af2.png"
+  },
+  {
+    "revision": "22f3cbdc8ed9568230d6710118d36600",
+    "url": "/static/media/OnlineShop2.22f3cbdc.png"
+  },
+  {
+    "revision": "a1940eec9190f42afd1a27b8d5647fe2",
+    "url": "/static/media/OnlineShop3.a1940eec.png"
+  },
+  {
+    "revision": "d324db565123451ce52498b21e9d16fd",
+    "url": "/static/media/OnlineShop4.d324db56.png"
+  },
+  {
+    "revision": "760f8ef132954a11dc0bc2a759040e1c",
+    "url": "/static/media/OnlineShop5.760f8ef1.png"
+  },
+  {
+    "revision": "715d97d860ce9e65d2909ac394b879b2",
+    "url": "/static/media/OnlineShop6.715d97d8.png"
+  },
+  {
+    "revision": "22a2cc7167023c75bf49042f633db472",
+    "url": "/static/media/OnlineShop7.22a2cc71.png"
+  },
+  {
+    "revision": "211229ab47c182ffa4a96f2411f00f56",
+    "url": "/static/media/Pastebill1.211229ab.png"
+  },
+  {
+    "revision": "1ec0c5f8085905916b76db62acacd515",
+    "url": "/static/media/Pastebill2.1ec0c5f8.png"
+  },
+  {
+    "revision": "8876c8cc6dd2b8a0220c1ea6dbc85c7a",
+    "url": "/static/media/Pastebill3.8876c8cc.png"
+  },
+  {
+    "revision": "1750770076e99d86f8542abc0116fcb1",
+    "url": "/static/media/Pastebill4.17507700.png"
+  },
+  {
+    "revision": "662ff6d414c9bbf70bd7ba4c77242470",
+    "url": "/static/media/Pastebill5.662ff6d4.png"
+  },
+  {
+    "revision": "1c2b985d0b40216360d7e61b31f7028c",
+    "url": "/static/media/app1.1c2b985d.png"
+  },
+  {
+    "revision": "62ad65e3ba1f409f1232be9b3cc191bf",
+    "url": "/static/media/app2.62ad65e3.png"
+  },
+  {
+    "revision": "b15267fe37e040286e976fed39d0bed1",
+    "url": "/static/media/app3.b15267fe.png"
+  },
+  {
+    "revision": "9758365b104a356b99c378404164abe8",
+    "url": "/static/media/app4.9758365b.png"
+  },
+  {
+    "revision": "4fdcc7b3b29d479340c72a86db1859e3",
+    "url": "/static/media/app5.4fdcc7b3.png"
+  },
+  {
+    "revision": "cfe4494c5fb585dbf3da7ef7ace5d7c5",
+    "url": "/static/media/app6.cfe4494c.png"
+  },
+  {
+    "revision": "d53efa42297192528596e3eb0f230f2f",
+    "url": "/static/media/app7.d53efa42.png"
+  },
+  {
+    "revision": "d0e2dcee79937ad327067e4954cbd5a8",
+    "url": "/static/media/app8.d0e2dcee.png"
+  },
+  {
+    "revision": "ff5567ced67dc566070ff6d38a4d81d2",
+    "url": "/static/media/unsplash2.ff5567ce.jpg"
+  },
+  {
+    "revision": "83847cc6505baa1be6902164168a3142",
+    "url": "/static/media/vova-avatar.83847cc6.jpg"
+  }
+]);
